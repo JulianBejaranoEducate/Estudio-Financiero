ee1faa69dc69603b6000a1a559803b336bbace17
Canquito <jbejarano@uniempresarial.edu.co>
2024-11-17 16:21:15 -0500

    Poyecto 5

---
+++ src/main/java/co/edu/estudiofinanciero/EstudioFinanciero.java
@@ -0,0 +1,13 @@
+package co.edu.estudiofinanciero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudioFinanciero {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudioFinanciero.class, args);
+	}
+
+}
